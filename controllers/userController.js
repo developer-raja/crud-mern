8fba2ee0f84c9bb3914a7d85781d3401cb8cc13c
raja <42689352+developer-raja@users.noreply.github.com>
2022-06-09 18:24:54 +0530

    remove-middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,7 @@
 const User = require("../models/userSchema");
-const asyncHandler = require("../middleware/async");
 
 // Post user data
-const postUser = asyncHandler(async (req, res) => {
+const postUser = async (req, res) => {
   const { name, email, age, mobile, work, add, desc } = req.body;
 
   if (!name || !email || !age || !mobile || !work || !add || !desc) {
@@ -25,30 +24,30 @@ const postUser = asyncHandler(async (req, res) => {
   } catch (error) {
     res.status(422).json({ msg: error });
   }
-});
+};
 
 // Get all user data
-const getUsers = asyncHandler(async (req, res) => {
+const getUsers = async (req, res) => {
   try {
     const userData = await User.find();
     res.status(200).json(userData);
   } catch (error) {
     res.status(422).json({ msg: error });
   }
-});
+};
 
 // Get user data by id
-const getUser = asyncHandler(async (req, res) => {
+const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     res.status(200).json(user);
   } catch (error) {
     res.status(422).json(error);
   }
-});
+};
 
 // Update user data by id
-const updateUser = asyncHandler(async (req, res) => {
+const updateUser = async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -57,17 +56,17 @@ const updateUser = asyncHandler(async (req, res) => {
   } catch (error) {
     res.status(422).json({ msg: error });
   }
-});
+};
 
 // Delete user by id
-const deleteUser = asyncHandler(async (req, res) => {
+const deleteUser = async (req, res) => {
   try {
     const deleteUser = await User.remove({ _id: req.params.id });
     res.status(200).json(deleteUser);
   } catch (error) {
     res.status(422).json({ msg: error });
   }
-});
+};
 
 module.exports = {
   postUser,
